9b740196a16027399f77ef512476c3093edb2933
Yves Van Broekhoven <yves@mrhenry.be>
2011-05-06 14:40:43 +0200

    Added jquery-reformer

---
+++ reg/jquery-reformer/1.0.0/lib/core.js
@@ -0,0 +1,41 @@
+var $     = require("jquery")
+;
+
+/*
+ * jQuery reFormer
+ *
+ * Version: 1.0.0
+ * Author: Yves Van Broekhoven
+ */
+
+var _submit
+,   _handleResponse
+,   _handleError
+;
+
+_submit = function(event){
+  var ctx = $(this);
+  event.preventDefault();
+  $.ajax({
+    type:     'POST',
+    url:      ctx.attr('action'),
+    data:     ctx.serialize(),
+    success:  function(data){
+      _handleResponse(data, ctx);
+    },
+    error:    _handleError
+  });
+};
+
+_handleResponse = function(data, ctx){
+  var form = $("#" + ctx[0].id, data).first();
+  ctx.replaceWith(form);
+};
+
+_handleError = function(){
+  console.log("something went wrong baby");
+};
+
+$.fn.reformer = function(){
+  return this.live("submit", _submit);
+};
