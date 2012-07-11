26ccf06e7ff3aee6c571709564f88926776ce170
Simon Menke <simon.menke@gmail.com>
2012-07-11 11:06:32 +0200

    Pass LINT

--- reg/jquery-mousewheel/3.0.2/lib/mousewheel.js
+++ reg/jquery-mousewheel/3.0.2/lib/mousewheel.js
@@ -5,7 +5,7 @@
  * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
  *
  * Version: 3.0.2
- * 
+ *
  * Requires: 1.2.2+
  */
 
@@ -20,17 +20,19 @@ var types = ['DOMMouseScroll', 'mousewheel']
 $.event.special.mousewheel = {
 	setup: function() {
 		if ( this.addEventListener ) {
-			for ( var i=types.length; i; ) {
+      var i;
+			for ( i=types.length; i; ) {
 				this.addEventListener( types[--i], handler, false );
 			}
 		} else {
 			this.onmousewheel = handler;
 		}
 	},
-	
+
 	teardown: function() {
 		if ( this.removeEventListener ) {
-			for ( var i=types.length; i; ) {
+      var i;
+			for ( i=types.length; i; ) {
 				this.removeEventListener( types[--i], handler, false );
 			}
 		} else {
@@ -43,7 +45,7 @@ $.fn.extend({
 	mousewheel: function(fn) {
 		return fn ? this.bind("mousewheel", fn) : this.trigger("mousewheel");
 	},
-	
+
 	unmousewheel: function(fn) {
 		return this.unbind("mousewheel", fn);
 	}
@@ -52,13 +54,13 @@ $.fn.extend({
 
 handler = function(event) {
 	var args = [].slice.call( arguments, 1 ), delta = 0, returnValue = true;
-	
+
 	event = $.event.fix(event || window.event);
 	event.type = "mousewheel";
-	
+
 	if ( event.wheelDelta ) { delta = event.wheelDelta/120; }
 	if ( event.detail     ) { delta = -event.detail/3; }
-	
+
 	// Add events and delta to the front of the arguments
 	args.unshift(event, delta);
 
